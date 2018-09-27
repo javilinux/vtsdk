da3daa3ae3c6dd300d851d0d270d376470a25a85
Javier Ramirez <jaramire@p50.sombrerorojo.com>
2018-09-27 22:34:21 +0200

    Solves #1

--- vt.py
+++ vt.py
@@ -9,31 +9,36 @@ from pygments import highlight
 from pygments.lexers.data import JsonLexer
 from pygments.formatters.terminal import TerminalFormatter
 with open('apikey.txt', 'r') as myfile:
-  myapikey = myfile.read().replace('\n','')
+    myapikey = myfile.read().replace('\n','')
 
 def menu():
-	"""
-	Function to clear screen and show menu
-	"""
-	os.system('clear') 
-        print ("Choose an option")
-	print ("\t1 - url report")
-	print ("\t9 - salir")
-         
-        # Requesting an option
-        optionMenu = raw_input("Choose option >> ")
+    """
+    Function to clear screen and show menu
+    """
+    os.system('clear')
+    print ("Choose an option")
+    print ("\t1 - url report")
+    print ("\t2 - domain report")
+    print ("\t9 - salir")
+        
+    # Requesting an option
+    optionMenu = raw_input("Choose option >> ")
 
-        if (optionMenu=="1"):
-            print ("")
-            resource = raw_input("Choose url:")
-            urlReport(resource)
-        elif (optionMenu=="9"):
-            print ("Pressed 9...\nExiting\n")
-            exit()
-        else:
-            print ("")
-            raw_input("No correct option selected...\npress any key to continue.")
-            menu()
+    if (optionMenu=="1"):
+        print ("")
+        resource = raw_input("Choose url:")
+        urlReport(resource)
+    elif (optionMenu=="2"):
+        print ("")
+        resource = raw_input("Choose domain:")
+        domainReport(resource)
+    elif (optionMenu=="9"):
+        print ("Pressed 9...\nExiting\n")
+        exit()
+    else:
+        print ("")
+        raw_input("No correct option selected...\npress any key to continue.")
+        menu()
 
 def responseParser(response):
     if response.status_code != 200 :
@@ -53,6 +58,18 @@ def urlReport(resource):
     url = 'https://www.virustotal.com/vtapi/v2/url/report'
     params = {'apikey': myapikey, 'resource':resource , 'allinfo':True, 'scan':'1'}
     response = requests.request('POST', url , params=params )
+    responseParser(response)
+    raw_input("Press any key to continue")
+    menu()
+
+def domainReport(resource):
+    """
+    Function to get an url report
+    Since we use scan=1 will automatically submit the URL for analysis if no report is found for it in VirusTotal's database.
+    """
+    url = 'https://www.virustotal.com/vtapi/v2/domain/report'
+    params = {'apikey': myapikey, 'domain':resource }
+    response = requests.request('GET', url , params=params )
     responseParser(response)
     raw_input("Press any key to continue")
     menu()
